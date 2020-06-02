f036f8748dba05512b5b571974419fe234276e35
github username <github email address>
2020-06-02 16:50:19 -0400

    added create action and strong params for brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -5,5 +5,17 @@ class BrandsController < ApplicationController
         render json: brands
     end 
 
+    def create 
+        brand = Brand.new(brand_params)
+        if brand.save 
+            render json: brand, status: :accepted 
+        else 
+            render json: {errors: brand.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
 
+    private
+    def brand_params
+        params.require(:brand).permit(:name)
+    end 
 end
